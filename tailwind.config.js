bd71489b289a592f08973b9f8053fe917e7a8695
BagasAlterra <bagasdhitya@alterra.id>
2023-08-07 12:34:08 +0700

    added theme colors & font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "health-blue-light": "#9CCBFB",
+        "health-blue-reguler": "#286DA8",
+        "health-blue-medium": "#1F4E78",
+        "health-blue-dark": "#004A7C",
+        "health-red-light": "#A82828",
+        "health-red-dark": "#6F1C1C",
+      },
+    },
   },
   plugins: [],
 };
