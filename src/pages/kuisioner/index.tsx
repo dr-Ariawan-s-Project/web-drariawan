0080288e9bfaf7897f77adfa9d36bf2058804fbf
BagasAlterra <bagas.dhitya@alterra.id>
2023-08-14 19:09:15 +0700

    added background image for more dynamic background

--- src/pages/kuisioner/index.tsx
+++ src/pages/kuisioner/index.tsx
@@ -1,10 +1,19 @@
 import { useState, useEffect } from "react"
 import { NavigateFunction, useNavigate } from "react-router-dom"
 
+import Background from "../../assets/illustrations/ariawan_background.jpg"
 import DoctorBlue from "../../assets/illustrations/ariawan_blue.svg"
 import ListQuestionaire from "../../assets/icons/list_kuisioner.svg"
 import FillQuestionaire from "../../assets/icons/fill_kuisioner.svg"
 import UnfillQuestionaire from "../../assets/icons/unfill_kuisioner.svg"
+
+const backgroundStyle = {
+    backgroundImage: `url(${Background})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundAttachment: "fixed",
+};
 
 const Kuisioner = () => {
 
@@ -22,9 +31,11 @@ const Kuisioner = () => {
     }, [])
 
     return (
-        <section className={`flex justify-center lg:items-center w-screen min-h-screen bg-white ${fadeIn ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'} transition-opacity duration-500 transform`}>
-            <div className="m-5">
-                <div className="grid grid-cols-1 gap-y-3 lg:mx-20">
+        <section
+            style={backgroundStyle}
+            className={`flex justify-center lg:items-center w-screen min-h-screen bg-white ${fadeIn ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'} transition-opacity duration-500 transform`}>
+            <div className="bg-white">
+                <div className="grid grid-cols-1 gap-y-3 m-10 lg:mx-20">
                     <h2>Pertanyaan</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
                     <div className="lg:flex lg:flex-row-reverse">
