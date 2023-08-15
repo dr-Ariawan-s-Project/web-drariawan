6d5d2a5f067fd691afdd3e6e4af037cef197ba3b
bagasdhitya98 <bagas.dhitya@alterra.id>
2023-08-15 11:27:24 +0700

    create for fetching data mechanism structure on zustand : user

---
+++ src/store/apiUser.ts
@@ -0,0 +1,60 @@
+import {create} from 'zustand'
+import axios from 'axios'
+
+import { UserState } from '../utils/api'
+
+export const useQuestionaire = create<UserState>((set) => ({
+    data: [],
+    loading: false,
+    error: null,
+    getUser: async () => {
+      set({ loading: true, error: null });
+      try {
+        const response = await axios.get('/user');
+        set({ data: response.data, loading: false });
+      } catch (error) {
+        set({ loading: false, error: 'Error fetching data' });
+      }
+    },
+    postUser: async (data) => {
+        set({ loading: true, error: null });
+        try {
+          const response = await axios.post('/user', data);
+          set({ data: response.data, loading: false });
+          return response.data;
+        } catch (error) {
+          set({ loading: false, error: 'Error posting data' });
+          throw error;
+        }
+    },
+    putUser: async (data) => {
+        set({ loading: true, error: null });
+        try {
+          const response = await axios.put('/user', data);
+          set({ data: response.data, loading: false });
+          return response.data;
+        } catch (error) {
+          set({ loading: false, error: 'Error edit data' });
+          throw error;
+        }
+    },
+    getList: async() => {
+        set({ loading: true, error: null });
+        try {
+          const response = await axios.get('/user/list');
+          set({ data: response.data, loading: false });
+        } catch (error) {
+          set({ loading: false, error: 'Error fetching data' });
+        }
+    },
+    postDeactivate: async(data) => {
+        set({ loading: true, error: null });
+        try {
+          const response = await axios.post('/user/deactivate', data);
+          set({ data: response.data, loading: false });
+          return response.data;
+        } catch (error) {
+          set({ loading: false, error: 'Error fetching data' });
+        }
+    }
+  }));
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -6,3 +6,14 @@ export type QuestionaireState = {
     postQuestionaire: (newData: any) => Promise<void>
     validateQuestionaire: (validateData: any) => Promise<void>
 }
+
+export type UserState = {
+    data: any[],
+    loading: boolean;
+    error: string | null;
+    getUser: () => Promise<void>,
+    getList: () => Promise<void>,
+    postUser: (data: any) => Promise<void>,
+    postDeactivate: (data: any) => Promise<void>
+    putUser: (data: any) => Promise<void>,
+}
