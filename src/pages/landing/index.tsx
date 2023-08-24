fe30da68c2f5988d67fc1cfdf03d4e74fff55e17
bagasdhitya98 <bagas.dhitya@alterra.id>
2023-08-24 13:56:29 +0700

    embrace pulse animation on splash screen before landing page session

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,76 +1,63 @@
-import { useState, useEffect } from "react"
-import { NavigateFunction, useNavigate } from "react-router-dom"
+import { useState, useEffect } from 'react';
+import { NavigateFunction, useNavigate } from 'react-router-dom';
 
-import LandingIllustration from "../../assets/illustrations/ariawan_ai.png"
-import data from '../../datas/landing/landing.json'
+import LandingIllustration from '../../assets/illustrations/ariawan_ai.png';
+import data from '../../datas/landing/landing.json';
 
-import Button from "../../components/Button"
-import AnimatedWrapper from "../../components/AnimatedWrapper"
+import Button from '../../components/Button';
 
 const Landing = () => {
+  const navigate: NavigateFunction = useNavigate();
+  const [fadeIn, setFadeIn] = useState<boolean>(false);
 
-    const navigate: NavigateFunction = useNavigate()
-    const [transition, setTransition] = useState<boolean>(true)
-    const [fadeIn, setFadeIn] = useState<boolean>(false)
+  useEffect(() => {
+    const fadeIn = setTimeout(() => {
+      setFadeIn(true);
+    }, 1600);
 
-    useEffect(() => {
-        const transition = setTimeout(() => {
-            setTransition(false)
-        }, 1500)
-        const fadeIn = setTimeout(() => {
-            setFadeIn(true)
-        }, 1600)
+    return () => {
+      clearTimeout(fadeIn);
+    };
+  }, []);
 
-        return () => {
-            clearTimeout(transition)
-            clearTimeout(fadeIn)
-        }
-    }, [])
+  return (
+    <section
+      className={`flex justify-center w-screen min-h-screen bg-health-blue-thin ${
+        fadeIn ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'
+      } transition-opacity duration-500 transform`}
+    >
+      <div className="mx-auto lg:flex lg:flex-row-reverse">
+        <div className="flex flex-col grid gap-y-5 text-center mx-10 bg-white rounded-md shadow-md lg:gap-y-20 lg:content-center lg:mx-20 my-10">
+          <div className="lg:grid lg:grid-cols-2 lg:gap-y-20 m-10">
+            <div className="lg:w-full lg:h-full lg:mb-0 mb-10">
+              <img
+                className="rounded-bb-left-right"
+                src={LandingIllustration}
+                alt="Landing Illustration"
+              />
+            </div>
+            <div className="lg:grid lg:gap-y-5 grid gap-y-10">
+              <h2 className="lg:text-left lg:mr-52">{data.title}</h2>
+              <p className="lg:text-justify">{data.description}</p>
+              <div className="grid gap-y-3 mt-10 lg:my-auto lg:text-left">
+                <p className="font-italic_medium">{data.name}</p>
+                <p className="font-semibold">{data.faculty}</p>
+              </div>
+              <div className="mx-5 lg:mx-0 mt-10 font-semibold lg:mt-auto">
+                <Button
+                  id="mulai"
+                  label="Mulai"
+                  type="blue"
+                  active={true}
+                  onClick={() => navigate('/kuisioner')}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-    console.log(transition)
-
-    return (
-        <>
-            {
-                transition === true ?
-                    <AnimatedWrapper>Loading ...</AnimatedWrapper> :
-                    <section
-                        className={`flex justify-center w-screen min-h-screen bg-health-blue-thin ${fadeIn ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'} transition-opacity duration-500 transform`}>
-                        <div className="mx-auto lg:flex lg:flex-row-reverse">
-                            <div className="flex flex-col grid gap-y-5 text-center mx-10 bg-white rounded-md shadow-md lg:gap-y-20 lg:content-center lg:mx-20 my-10">
-                                <div className="lg:grid lg:grid-cols-2 lg:gap-y-20 m-10">
-                                    <div className="lg:w-full lg:h-full lg:mb-0 mb-10">
-                                        <img
-                                            className="rounded-bb-left-right"
-                                            src={LandingIllustration}
-                                            alt="Landing Illustration"
-                                        />
-                                    </div>
-                                    <div className="lg:grid lg:gap-y-5 grid gap-y-10">
-                                        <h2 className="lg:text-left lg:mr-52">{data.title}</h2>
-                                        <p className="lg:text-justify">{data.description}</p>
-                                        <div className="grid gap-y-3 mt-10 lg:my-auto lg:text-left">
-                                            <p className="font-italic_medium">{data.name}</p>
-                                            <p className="font-semibold">{data.faculty}</p>
-                                        </div>
-                                        <div className="mx-5 lg:mx-0 mt-10 font-semibold lg:mt-auto">
-                                            <Button
-                                                id="mulai"
-                                                label="Mulai"
-                                                type="blue"
-                                                active={true}
-                                                onClick={() => navigate('/kuisioner')}
-                                            />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-            }
-        </>
-
-    )
-}
-
-export default Landing
+export default Landing;
--- src/pages/splash/index.tsx
+++ src/pages/splash/index.tsx
@@ -1,27 +1,60 @@
-import { useEffect } from "react";
-import { useNavigate, NavigateFunction } from "react-router-dom";
-
-import DoctorWalk from '../../assets/animations/doctor_walking.gif'
+import { useState, useEffect } from 'react';
+import { NavigateFunction, useNavigate } from 'react-router-dom';
+import '../../styles/pulse.css';
+import classNames from 'classnames';
 
 const Splash = () => {
-    const navigate: NavigateFunction = useNavigate();
+  const navigate: NavigateFunction = useNavigate();
+  const [pulseIndex, setPulseIndex] = useState(0);
+  const [typingText, setTypingText] = useState('');
+  const textToType = "Welcome to dr Ariawan's Questioner  ";
+  const pulseSizes = [60, 80, 100];
+  const typingSpeed = 100;
 
-    useEffect(() => {
-        const timeout = setTimeout(() => {
-            navigate("/landing");
-        }, 2500);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setPulseIndex((prevIndex) => (prevIndex + 1) % 3);
+    }, 500);
 
-        return () => clearTimeout(timeout);
-    }, [navigate]);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
-    return (
-        <div className="flex items-center justify-center w-screen h-screen bg-white">
-            <div className="text-health-blue-reguler slide-in-right">
-                <img src={DoctorWalk} width={300} height={300} />
-                <h2 className="mt-20">Welcome to My Health</h2>
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    let currentIndex = 0;
+    const typingInterval = setInterval(() => {
+      setTypingText(textToType.substring(0, currentIndex));
+      currentIndex++;
+      console.log(textToType.length);
+      if (currentIndex >= textToType.length) {
+        navigate('/landing');
+      }
+    }, typingSpeed);
+
+    return () => {
+      clearInterval(typingInterval);
+    };
+  }, []);
+
+  return (
+    <div className="w-screen h-screen bg-health-blue-dark flex flex-col justify-center items-center">
+      <div className="grid grid-cols-3 gap-x-10 mb-20">
+        {pulseSizes.map((size, index) => (
+          <div
+            key={index}
+            className={classNames('heart', {
+              pulse: index === pulseIndex,
+            })}
+            style={{ width: size, height: size }}
+          />
+        ))}
+      </div>
+      <div className="text-white font-semibold">
+        <h2>{typingText}</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Splash;
