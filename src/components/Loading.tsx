283d45d30f071c8abaed979c7f1267bf9862b8dc
bagasdhitya98 <bagas.dhitya@alterra.id>
2023-08-25 12:16:57 +0700

    integrate api server for get questioner on kuisioner page

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,19 +1,21 @@
-import { FC } from "react";
+import { FC } from 'react';
 
-import DoctorWalk from '../assets/animations/doctor_walking.gif'
-import Modal from "./Modal";
+import DoctorWalk from '../assets/animations/doctor_walking.gif';
+import Modal from './Modal';
 
-import { LoadingProps } from "../utils/component";
+import { LoadingProps } from '../utils/component';
 
 const Loading: FC<LoadingProps> = ({ id, isOpen }) => {
-    return (
-        <Modal id={id} isOpen={isOpen}>
-            <div className="p-10 grid place-items-center gap-y-10">
-                <img src={DoctorWalk} width={200} height={200} />
-                <p className="text-health-blue-dark font-semibold">Loading ...</p>
-            </div>
-        </Modal>
-    )
-}
+  return (
+    <Modal id={id} isOpen={isOpen}>
+      <div className="p-10 grid place-items-center gap-y-10">
+        <img src={DoctorWalk} width={200} height={200} />
+        <p className="text-health-blue-dark font-semibold">
+          Tunggu Sebentar ...
+        </p>
+      </div>
+    </Modal>
+  );
+};
 
-export default Loading
+export default Loading;
--- src/pages/kuisioner/index.tsx
+++ src/pages/kuisioner/index.tsx
@@ -1,95 +1,93 @@
-import { useState, useEffect } from "react"
-import { NavigateFunction, useNavigate } from "react-router-dom"
+import { useState, useEffect } from 'react';
+import { NavigateFunction, useNavigate } from 'react-router-dom';
 
-import DoctorBlue from "../../assets/illustrations/ariawan_blue.svg"
-import ListQuestionaire from "../../assets/icons/list_kuisioner.svg"
-import FillQuestionaire from "../../assets/icons/fill_kuisioner.svg"
-import UnfillQuestionaire from "../../assets/icons/unfill_kuisioner.svg"
-import DoctorCircle from "../../assets/illustrations/ariawan_ai.png"
+import DoctorBlue from '../../assets/illustrations/ariawan_blue.svg';
+import ListQuestionaire from '../../assets/icons/list_kuisioner.svg';
+import FillQuestionaire from '../../assets/icons/fill_kuisioner.svg';
+import UnfillQuestionaire from '../../assets/icons/unfill_kuisioner.svg';
 
-import AnimatedWrapper from "../../components/AnimatedWrapper"
-import RotatingCircle from "../../components/RotatingCircle"
+import { useQuestionaire } from '../../store/apiQuestionaire';
+
+import AnimatedWrapper from '../../components/AnimatedWrapper';
+import Loading from '../../components/Loading';
 
 const Kuisioner = () => {
+  const navigate: NavigateFunction = useNavigate();
+  const { data, error } = useQuestionaire() as any;
 
-    const navigate: NavigateFunction = useNavigate()
-    const [transition, setTransition] = useState<boolean>(true)
-    const [fadeIn, setFadeIn] = useState<boolean>(false)
+  const [transition, setTransition] = useState<boolean>(true);
+  const [fadeIn, setFadeIn] = useState<boolean>(false);
 
-    useEffect(() => {
-        const transition = setTimeout(() => {
-            setTransition(false)
-        }, 1500)
-        const fadeIn = setTimeout(() => {
-            setFadeIn(true)
-        }, 1600)
+  useEffect(() => {
+    const transition = setTimeout(() => {
+      setTransition(false);
+    }, 1500);
+    const fadeIn = setTimeout(() => {
+      setFadeIn(true);
+    }, 1600);
 
-        return () => {
-            clearTimeout(transition)
-            clearTimeout(fadeIn)
-        }
-    }, [])
+    return () => {
+      clearTimeout(transition);
+      clearTimeout(fadeIn);
+    };
+  }, []);
 
-    return (
-        <>
-            {
-                transition === true ?
-                    <AnimatedWrapper>Loading ...</AnimatedWrapper> :
-                    <section
-                        className={`flex justify-center lg:items-center w-screen min-h-screen bg-health-blue-thin ${fadeIn ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'} transition-opacity duration-500 transform`}>
-                        <RotatingCircle image={DoctorCircle} size={100} />
-                        <div className="bg-white">
-                            <div className="grid grid-cols-1 gap-y-3 m-10 lg:mx-20">
-                                <h2>Pertanyaan</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-                                <div className="lg:flex lg:flex-row-reverse">
-                                    <div className="mt-10 bg-health-blue-thin flex justify-center rounded-lg lg:ml-20 lg:w-full">
-                                        <img src={DoctorBlue} />
-                                    </div>
-                                    <div className="lg:grid lg:grid-cols-1">
-                                        <div className="flex mt-10 cursor-pointer hover:text-health-blue-thin" onClick={() => navigate(`/kuisioner/${'Pertanyaan_1'}`)}>
-                                            <img src={ListQuestionaire} width={40} height={40} alt="" />
-                                            <div className="mx-5">
-                                                <h2>Pertanyaan 1</h2>
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-                                            </div>
-                                            <img src={FillQuestionaire} width={40} height={40} alt="" />
-                                        </div>
-                                        <div className="flex mt-10 cursor-pointer hover:text-health-blue-thin" onClick={() => navigate(`/kuisioner/${'Pertanyaan_1'}`)}>
-                                            <img src={ListQuestionaire} width={40} height={40} alt="" />
-                                            <div className="mx-5">
-                                                <h2>Pertanyaan 2</h2>
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-                                            </div>
-                                            <img src={UnfillQuestionaire} width={40} height={40} alt="" />
-                                        </div>
-                                        <div className="flex mt-10 cursor-pointer hover:text-health-blue-thin" onClick={() => navigate(`/kuisioner/${'Pertanyaan_1'}`)}>
-                                            <img src={ListQuestionaire} width={40} height={40} alt="" />
-                                            <div className="mx-5">
-                                                <h2>Pertanyaan 3</h2>
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-                                            </div>
-                                            <img src={UnfillQuestionaire} width={40} height={40} alt="" />
-                                        </div>
-                                        <div className="flex mt-10 cursor-pointer hover:text-health-blue-thin" onClick={() => navigate(`/kuisioner/${'Pertanyaan_1'}`)}>
-                                            <img src={ListQuestionaire} width={40} height={40} alt="" />
-                                            <div className="mx-5">
-                                                <h2>Pertanyaan 4</h2>
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-                                            </div>
-                                            <img src={UnfillQuestionaire} width={40} height={40} alt="" />
-                                        </div>
-                                    </div>
+  console.log(data);
 
-                                </div>
-                            </div>
+  return (
+    <>
+      {transition === true ? (
+        <AnimatedWrapper>Loading ...</AnimatedWrapper>
+      ) : (
+        <section
+          className={`flex justify-center lg:items-center w-screen min-h-screen bg-health-blue-thin ${
+            fadeIn ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'
+          } transition-opacity duration-500 transform`}
+        >
+          <div className="bg-white rounded-md shadow-md my-auto">
+            <div className="grid grid-cols-1 gap-y-3 m-10 lg:mx-20">
+              <h2>Pertanyaan</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+              <div className="lg:flex lg:flex-row-reverse">
+                <div className="mt-10 bg-health-blue-thin flex justify-center rounded-lg lg:ml-20 lg:w-full">
+                  <img src={DoctorBlue} alt="Doctor Illustration" />
+                </div>
+                <div className="lg:grid lg:grid-cols-1">
+                  {data?.data?.map((item: any, index: any) => {
+                    return (
+                      <div
+                        key={index}
+                        className="flex mt-10 cursor-pointer hover:text-health-blue-thin"
+                        onClick={() => navigate(`/kuisioner/${'Pertanyaan_1'}`)}
+                      >
+                        <img
+                          src={ListQuestionaire}
+                          width={40}
+                          height={40}
+                          alt="List Icon"
+                        />
+                        <div className="mx-5">
+                          <h2>{item?.description}</h2>
+                          <p>Tipe Kuisioner : {item?.type}</p>
                         </div>
-                        <RotatingCircle image={DoctorCircle} size={100} />
-                    </section>
-            }
-        </>
+                        <img
+                          src={UnfillQuestionaire}
+                          width={40}
+                          height={40}
+                          alt="Unfill Icon"
+                        />
+                      </div>
+                    );
+                  })}
+                  {error === null && <Loading id="loading" isOpen={true} />}
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      )}
+    </>
+  );
+};
 
-    )
-}
-
-export default Kuisioner
+export default Kuisioner;
