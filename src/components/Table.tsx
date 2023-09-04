d0212410ee235c8d008b6851075bc74bd826002b
bagasdhitya98 <bagas.dhitya@alterra.id>
2023-09-04 20:52:26 +0700

    create chart and table for admin dash

---
+++ src/components/Table.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { useTable } from 'react-table';
+
+const Table = () => {
+  const data: any = React.useMemo(
+    () => [
+      {
+        col1: 'Hello',
+        col2: 'World',
+      },
+      {
+        col1: 'React',
+        col2: 'Table',
+      },
+    ],
+    []
+  );
+
+  const columns: any = React.useMemo(
+    () => [
+      {
+        Header: 'Column 1',
+        accessor: 'col1',
+      },
+      {
+        Header: 'Column 2',
+        accessor: 'col2',
+      },
+    ],
+    []
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({
+      columns,
+      data,
+    });
+
+  return (
+    <div>
+      <h2>Table</h2>
+      <table {...getTableProps()} className="table-auto">
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
