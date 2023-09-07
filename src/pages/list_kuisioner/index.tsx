b224def5add3632e38b50e21dabf04578d87fa75
bagasdhitya98 <bagas.dhitya@alterra.id>
2023-09-07 08:56:48 +0700

    integrate questioner on list kuisioner

--- src/pages/list_kuisioner/index.tsx
+++ src/pages/list_kuisioner/index.tsx
@@ -2,17 +2,19 @@ import { useState, useEffect } from 'react';
 
 import DoctorBlue from '../../assets/illustrations/ariawan_blue.svg';
 import DoctorWhite from '../../assets/illustrations/ariawan_white.svg';
-
 import Card from '../../components/Card';
 
+import { useQuestionaire } from '../../store/apiQuestionaire';
+
 const ListKuisioner = () => {
+  const { data, getQuestionaire } = useQuestionaire() as any;
   const [fadeIn, setFadeIn] = useState<boolean>(false);
 
   useEffect(() => {
     const timeout = setTimeout(() => {
       setFadeIn(true);
     }, 300);
-
+    getQuestionaire();
     return () => {
       clearTimeout(timeout);
     };
@@ -20,55 +22,35 @@ const ListKuisioner = () => {
 
   return (
     <section
-      className={`flex justify-center h-96 ml-60 ${
+      className={`flex justify-center ml-60 h-full mt-24 z-5 ${
         fadeIn ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'
       } transition-opacity duration-500 transform`}
     >
-      <div className="grid grid-cols-2 gap-y-5 gap-x-5 content-center lg:mx-auto lg:my-10">
-        <div className="lg:w-max lg:h-max">
-          <Card id="card" type="secondary">
-            <div className="lg:grid lg:grid-cols-2">
-              <div className="text-left lg:grid lg:content-center">
-                <p className="font-semibold">Kuisioner 1</p>
-                <p>Lorem Ipsum dolor sit amet </p>
-              </div>
-              <img className="my-5" src={DoctorBlue} />
+      <div className="lg:grid lg:grid-cols-2 lg:gap-y-5 grid grid-cols-1 lg:content-center lg:mx-20 lg:mt-5">
+        {data?.data?.map((item: any) => {
+          return (
+            <div className=" lg:h-max mx-5 my-3 lg:my-0">
+              <Card
+                id="card"
+                type={item?.id % 2 === 0 ? 'primary' : 'secondary'}
+              >
+                <div className="lg:grid lg:grid-cols-2">
+                  <div className="text-left lg:grid lg:content-center">
+                    <p className="font-semibold">{item?.description}</p>
+                    <p>
+                      Tipe Kuisioner :{' '}
+                      {item?.type === 'text' ? 'Teks' : 'Pilihan Ganda'}{' '}
+                    </p>
+                  </div>
+                  <img
+                    className="my-5"
+                    src={item?.id % 2 === 0 ? DoctorWhite : DoctorBlue}
+                  />
+                </div>
+              </Card>
             </div>
-          </Card>
-        </div>
-        <div className="lg:w-max lg:h-max">
-          <Card id="card" type="primary">
-            <div className="lg:grid lg:grid-cols-2">
-              <div className="text-left lg:grid lg:content-center">
-                <p className="font-semibold">Kuisioner 1</p>
-                <p>Lorem Ipsum dolor sit amet </p>
-              </div>
-              <img className="my-5" src={DoctorWhite} />
-            </div>
-          </Card>
-        </div>
-        <div className="lg:w-max lg:h-max">
-          <Card id="card" type="primary">
-            <div className="lg:grid lg:grid-cols-2">
-              <div className="text-left lg:grid lg:content-center">
-                <p className="font-semibold">Kuisioner 1</p>
-                <p>Lorem Ipsum dolor sit amet </p>
-              </div>
-              <img className="my-5" src={DoctorWhite} />
-            </div>
-          </Card>
-        </div>
-        <div className="lg:w-max lg:h-max">
-          <Card id="card" type="secondary">
-            <div className="lg:grid lg:grid-cols-2">
-              <div className="text-left lg:grid lg:content-center">
-                <p className="font-semibold">Kuisioner 1</p>
-                <p>Lorem Ipsum dolor sit amet </p>
-              </div>
-              <img className="my-5" src={DoctorBlue} />
-            </div>
-          </Card>
-        </div>
+          );
+        })}
       </div>
     </section>
   );
