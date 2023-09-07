a3c4bba207f70ca96084f8772fdfeba088d3a7ac
bagasdhitya98 <bagas.dhitya@alterra.id>
2023-09-07 08:55:21 +0700

    improvement sidebar and navbar effect on small device

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,6 +14,7 @@ const Navbar: FC<NavbarProps> = ({ page }) => {
       icon: 'warning',
       showCancelButton: true,
       confirmButtonText: 'Logout',
+      confirmButtonColor: '#004A7C',
       cancelButtonText: 'Batal',
       reverseButtons: true,
     }).then((result) => {
@@ -21,6 +22,7 @@ const Navbar: FC<NavbarProps> = ({ page }) => {
         Swal.fire({
           title: 'Sukses logout',
           icon: 'success',
+          confirmButtonColor: '#004A7C',
         }).then((result) => {
           if (result.isConfirmed) {
             console.log('Logout berhasil');
@@ -31,14 +33,14 @@ const Navbar: FC<NavbarProps> = ({ page }) => {
   };
 
   return (
-    <div className="bg-white shadow-md h-20 flex justify-between">
-      <p className="font-semibold p-6 ml-52">{page}</p>
+    <div className="bg-white shadow-md h-20 flex justify-between items-center">
+      <p className="font-semibold px-6 lg:ml-52 ml-20">{page}</p>
       <a
-        className="font-semibold text-health-blue-dark hover:text-health-blue-reguler cursor-pointer p-6 flex gap-x-5"
+        className="font-semibold text-health-blue-dark hover:text-health-blue-reguler cursor-pointer p-6 flex gap-x-2 md:gap-x-5"
         onClick={() => handleLogout()}
       >
-        <UserIcon />
-        <p className="my-auto">Hello, John Doe</p>
+        <UserIcon className="h-6 w-6 md:h-8 md:w-8" />
+        <p className="my-auto text-sm md:text-base">Hello, John Doe</p>
       </a>
     </div>
   );
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,17 +1,41 @@
+import { Bars3Icon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
+
 const Sidebar = () => {
+  const [toggle, setToggle] = useState<boolean>(false);
+
+  const sidebarToggle = () => {
+    setToggle(!toggle);
+  };
+
   const navigateTo = (path: any) => {
     window.location.href = path;
   };
 
   return (
-    <div className="bg-health-blue-dark text-white h-screen p-5 flex flex-col">
-      <h2
-        className="text-2xl text-center p-4 cursor-pointer"
-        onClick={() => navigateTo('/admin/')}
+    <div className={`bg-health-blue-dark text-white h-full p-5 flex flex-col`}>
+      <div className="flex flex-row-reverse">
+        <h2
+          className={`text-2xl text-center p-4 cursor-pointer lg:block  ${
+            toggle === true ? 'block' : 'hidden'
+          }`}
+          onClick={() => navigateTo('/admin/')}
+        >
+          Dashboard
+        </h2>
+        <a
+          className="flex justify-center items-center lg:hidden block cursor-pointer"
+          onClick={() => sidebarToggle()}
+        >
+          <Bars3Icon color={'white'} width={40} height={40} />
+        </a>
+      </div>
+
+      <nav
+        className={`flex-1 transition-all duration-300 ease-in-out lg:block ${
+          toggle === true ? 'block' : 'hidden'
+        }`}
       >
-        Dashboard
-      </h2>
-      <nav className="flex-1">
         <ul className="p-4">
           <li className="mb-2">
             <button
