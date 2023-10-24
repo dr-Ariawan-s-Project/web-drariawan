5abd525349da52c05422f16b6b45c6502437f7e5
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2023-10-24 18:07:17 +0700

    add: integration schedule list on table

--- src/pages/scheduling/index.tsx
+++ src/pages/scheduling/index.tsx
@@ -1,15 +1,19 @@
+import 'react-datepicker/dist/react-datepicker.css';
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+import Cookies from 'js-cookie';
 import Swal from 'sweetalert2';
 import axios from 'axios';
-import Cookies from 'js-cookie';
+
+import Modal from '../../components/Modal';
 
 const Scheduling = () => {
   const navigate = useNavigate();
   const token = Cookies.get('token');
-  const [reserve, setReserve] = useState([]);
+  const [reserve, setReserve] = useState<[]>([]);
+  const [open, setOpen] = useState<boolean>(false);
+  const [schedule, setSchedule] = useState<any>();
   const [selectedStartDate, setSelectedStartDate] = useState<any>(new Date());
 
   const daysOfWeek = [
@@ -40,7 +44,11 @@ const Scheduling = () => {
       const response = await axios.get('/v1/schedule/list');
       setReserve(response?.data?.data?.data);
     } catch (error) {
-      console.log(error);
+      Swal.fire({
+        title: 'Gagal',
+        text: `Gagal mengambil data, silahkan refresh halaman ini`,
+        confirmButtonText: 'OK',
+      });
     }
   };
 
@@ -60,11 +68,23 @@ const Scheduling = () => {
   selectedEndDate.setDate(selectedEndDate.getDate() + 6);
 
   const handleSelectSchedule = (day: string, time: string) => {
-    Swal.fire({
-      title: 'Sukses',
-      text: `Sukses menambahkan jadwal, giliran anda ${day} dan ${time}`,
-      confirmButtonText: 'OK',
+    const selectedData: any = reserve.find((schedule: any) => {
+      return (
+        schedule.day === day &&
+        time >= schedule.time_start &&
+        time < schedule.time_end
+      );
     });
+    const schedule = {
+      day: selectedData?.day,
+      time_start: selectedData?.time_start,
+      time_end: selectedData?.time_end,
+      health_care_address: selectedData?.health_care_address,
+      doctor_name: selectedData?.User?.Name,
+      doctor_specialization: selectedData?.User?.Specialization,
+    };
+    setSchedule(schedule);
+    setOpen(true);
   };
 
   useEffect(() => {
@@ -117,19 +137,70 @@ const Scheduling = () => {
       {timeSlots.map((time) => (
         <div key={time} className="flex mx-auto">
           <div className="w-20 p-2">{time}</div>
-          {daysOfWeek.map((day) => (
-            <div
-              key={day + time}
-              className={`w-24 p-2 justify-center border border-health-blue-thin cursor-pointer hover-bg-gray-200 ${
-                isScheduleBooked(day, time) ? 'bg-green-300' : ''
-              }`}
-              onClick={() => handleSelectSchedule(day, time)}
-            >
-              {isScheduleBooked(day, time) ? 'Sudah dipesan' : 'Tersedia'}
-            </div>
-          ))}
+          {daysOfWeek.map((day) => {
+            const selectedData: any = reserve.find((schedule: any) => {
+              return (
+                schedule.day === day &&
+                time >= schedule.time_start &&
+                time < schedule.time_end
+              );
+            });
+
+            const doctorColorClass = selectedData
+              ? selectedData.User.Specialization === 'obgyn'
+                ? 'bg-green-300'
+                : 'bg-blue-300'
+              : '';
+
+            return (
+              <div
+                key={day + time}
+                className={`w-24 p-2 justify-center border border-health-blue-thin cursor-pointer hover-bg-gray-200 ${
+                  isScheduleBooked(day, time) ? doctorColorClass : ''
+                }`}
+                onClick={() => handleSelectSchedule(day, time)}
+              >
+                {isScheduleBooked(day, time) ? 'Tersedia' : ''}
+              </div>
+            );
+          })}
         </div>
       ))}
+
+      <Modal id="schedule" isOpen={open} onClose={() => setOpen(false)}>
+        <div className="flex flex-col justify-center items-center">
+          <h2 className="font-semibold my-10">Detail Jadwal</h2>
+          <div className="h-0 w-full border-t-2 border-slate-200" />
+          <div className="flex flex-col my-10 gap-y-5 px-4">
+            <div className="flex gap-x-40">
+              <p className="w-1/3">Nama Dokter</p>
+              <p className="w-2/3">{schedule?.doctor_name}</p>
+            </div>
+            <div className="flex gap-x-40">
+              <p className="w-1/3">Spesialis</p>
+              <p className="w-2/3">
+                {schedule?.doctor_specialization
+                  ? schedule?.doctor_specialization
+                  : 'N/A'}
+              </p>
+            </div>
+            <div className="flex gap-x-40">
+              <p className="w-1/3">Alamat Praktik</p>
+              <p className="w-2/3">{schedule?.health_care_address}</p>
+            </div>
+            <div className="flex gap-x-40">
+              <p className="w-1/3">Hari Periksa</p>
+              <p className="w-2/3">{schedule?.day}</p>
+            </div>
+            <div className="flex gap-x-40">
+              <p className="w-1/3">Jam Periksa</p>
+              <p className="w-2/3">
+                {schedule?.time_start} - {schedule?.time_end}
+              </p>
+            </div>
+          </div>
+        </div>
+      </Modal>
     </section>
   );
 };
