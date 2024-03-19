d0affb0ddecc42e8f85b951a36b713dcf70f1676
Yoga S Devanada <ydevanada@gmail.com>
2024-03-19 15:09:15 +0700

    feat: add toast when user not authorized to access certain pages

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -1,65 +1,65 @@
-import * as React from 'react';
-import { ChevronLeft, ChevronRight, MoreHorizontal } from 'lucide-react';
+import * as React from "react";
+import { ChevronLeft, ChevronRight, MoreHorizontal } from "lucide-react";
 
-import { cn } from '@/lib/utils';
-import { ButtonProps, buttonVariants } from '@/components/ui/button';
+import { cn } from "@/lib/utils";
+import { ButtonProps, buttonVariants } from "@/components/ui/button";
 
-const Pagination = ({ className, ...props }: React.ComponentProps<'nav'>) => (
+const Pagination = ({ className, ...props }: React.ComponentProps<"nav">) => (
   <nav
     role="navigation"
     aria-label="pagination"
-    className={cn('mx-auto flex w-full justify-center', className)}
+    className={cn("mx-auto flex w-full justify-center relative", className)}
     {...props}
   />
 );
 
 const PaginationContent = React.forwardRef<
   HTMLUListElement,
-  React.ComponentProps<'ul'>
+  React.ComponentProps<"ul">
 >(({ className, ...props }, ref) => (
   <ul
     ref={ref}
-    className={cn('flex flex-row items-center gap-1', className)}
+    className={cn("flex flex-row items-center gap-1", className)}
     {...props}
   />
 ));
-PaginationContent.displayName = 'PaginationContent';
+PaginationContent.displayName = "PaginationContent";
 
 const PaginationItem = React.forwardRef<
   HTMLLIElement,
-  React.ComponentProps<'li'>
+  React.ComponentProps<"li">
 >(({ className, ...props }, ref) => (
-  <li ref={ref} className={cn('', className)} {...props} />
+  <li ref={ref} className={cn("", className)} {...props} />
 ));
-PaginationItem.displayName = 'PaginationItem';
+PaginationItem.displayName = "PaginationItem";
 
 type PaginationLinkProps = {
   isActive?: boolean;
-} & Pick<ButtonProps, 'size'> &
-  React.ComponentProps<'a'>;
+} & Pick<ButtonProps, "size"> &
+  React.ComponentProps<"a">;
 
 const PaginationLink = ({
   className,
   isActive,
-  size = 'icon',
+  size = "icon",
   ...props
 }: PaginationLinkProps) => (
   <PaginationItem>
     <a
-      aria-current={isActive ? 'page' : undefined}
+      aria-current={isActive ? "page" : undefined}
       className={cn(
         buttonVariants({
-          variant: isActive ? 'outline' : 'ghost',
+          variant: isActive ? "outline" : "ghost",
           size,
         }),
-        !props.href && 'cursor-not-allowed',
+        !props.href && "cursor-not-allowed",
         className
       )}
       {...props}
     />
   </PaginationItem>
 );
-PaginationLink.displayName = 'PaginationLink';
+PaginationLink.displayName = "PaginationLink";
 
 const PaginationPrevious = ({
   className,
@@ -69,8 +69,8 @@ const PaginationPrevious = ({
     aria-label="Go to previous page"
     size="default"
     className={cn(
-      'gap-1 pl-2.5',
-      !props.href && 'cursor-not-allowed',
+      "gap-1 pl-2.5",
+      !props.href && "cursor-not-allowed",
       className
     )}
     {...props}
@@ -79,7 +79,7 @@ const PaginationPrevious = ({
     <span>Previous</span>
   </PaginationLink>
 );
-PaginationPrevious.displayName = 'PaginationPrevious';
+PaginationPrevious.displayName = "PaginationPrevious";
 
 const PaginationNext = ({
   className,
@@ -89,8 +89,8 @@ const PaginationNext = ({
     aria-label="Go to next page"
     size="default"
     className={cn(
-      'gap-1 pr-2.5',
-      !props.href && 'cursor-not-allowed',
+      "gap-1 pr-2.5",
+      !props.href && "cursor-not-allowed",
       className
     )}
     {...props}
@@ -103,10 +103,10 @@ const PaginationNext = ({
 const PaginationEllipsis = ({
   className,
   ...props
-}: React.ComponentProps<'span'>) => (
+}: React.ComponentProps<"span">) => (
   <span
     aria-hidden
-    className={cn('flex h-9 w-9 items-center justify-center', className)}
+    className={cn("flex h-9 w-9 items-center justify-center", className)}
     {...props}
   >
     <MoreHorizontal className="h-4 w-4" />
--- src/routes/protected-route.tsx
+++ src/routes/protected-route.tsx
@@ -1,65 +1,68 @@
-import { Navigate, Outlet, useLocation, useParams } from 'react-router-dom';
+import { Navigate, Outlet, useLocation, useParams } from "react-router-dom";
 
-import { useAuthStore } from '@/utils/states';
+import { useToast } from "@/components/ui/use-toast";
+
+import { useAuthStore } from "@/utils/states";
 
 const nonLoggedInAccess = [
-  '/',
-  '/login',
-  '/register',
-  '/dashboard/login',
-  '/questionnaire',
-  '/questionnaire/form',
-  '/questionnaire/sent',
-  '/questionnaire/start',
-  '/questionnaire/finish',
+  "/",
+  "/login",
+  "/register",
+  "/dashboard/login",
+  "/questionnaire",
+  "/questionnaire/form",
+  "/questionnaire/sent",
+  "/questionnaire/start",
+  "/questionnaire/finish",
 ];
 
 const routeWhitelist: Record<string, string[]> = {
   suster: [
-    '/dashboard',
-    '/dashboard/schedules',
-    '/dashboard/books',
-    '/dashboard/settings',
+    "/dashboard",
+    "/dashboard/schedules",
+    "/dashboard/books",
+    "/dashboard/settings",
   ],
   dokter: [
-    '/dashboard',
-    '/dashboard/schedules',
-    '/dashboard/books',
-    '/dashboard/questionnaires',
-    '/dashboard/settings',
+    "/dashboard",
+    "/dashboard/schedules",
+    "/dashboard/books",
+    "/dashboard/questionnaires",
+    "/dashboard/settings",
   ],
   admin: [
-    '/dashboard',
-    '/dashboard/patients',
-    '/dashboard/schedules',
-    '/dashboard/questionnaires',
-    '/dashboard/settings',
+    "/dashboard",
+    "/dashboard/patients",
+    "/dashboard/schedules",
+    "/dashboard/questionnaires",
+    "/dashboard/settings",
   ],
   superadmin: [
-    '/dashboard',
-    '/dashboard/users',
-    '/dashboard/patients',
-    '/dashboard/schedules',
-    '/dashboard/questionnaires',
-    '/dashboard/settings',
+    "/dashboard",
+    "/dashboard/users",
+    "/dashboard/patients",
+    "/dashboard/schedules",
+    "/dashboard/questionnaires",
+    "/dashboard/settings",
   ],
   patient: [
-    '/',
-    '/profile',
-    '/scheduling',
-    '/scheduling/success',
-    '/scheduling/my-list',
-    '/questionnaire',
-    '/questionnaire/form',
-    '/questionnaire/sent',
-    '/questionnaire/start',
-    '/questionnaire/finish',
+    "/",
+    "/profile",
+    "/scheduling",
+    "/scheduling/success",
+    "/scheduling/my-list",
+    "/questionnaire",
+    "/questionnaire/form",
+    "/questionnaire/sent",
+    "/questionnaire/start",
+    "/questionnaire/finish",
   ],
 };
 
 const ProtectedRoute = () => {
   const { token, role } = useAuthStore((state) => state);
   const { pathname } = useLocation();
+  const { toast } = useToast();
   const params = useParams();
 
   if (token) {
@@ -68,7 +71,15 @@ const ProtectedRoute = () => {
     }
 
     if (!routeWhitelist[role].includes(pathname)) {
-      if (role === 'patient') return <Navigate to="/" />;
+      toast({
+        description:
+          "Anda tidak memiliki akses ke halaman ini, silahkan logout terlebih dahulu",
+        variant: "destructive",
+      });
+
+      if (role === "patient") {
+        return <Navigate to="/" />;
+      }
 
       return <Navigate to="/dashboard" />;
     } else {
