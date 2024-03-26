6655e8ec86fcb4e34f4a8378659f75e5332c2cf4
Yoga S Devanada <ydevanada@gmail.com>
2024-03-26 11:48:47 +0700

    feat: add cancel booking

--- src/utils/apis/books/api.ts
+++ src/utils/apis/books/api.ts
@@ -1,12 +1,12 @@
-import { IBook, BookingSchema } from './types';
-import axiosWithConfig from '@/utils/apis/axiosWithConfig';
-import { Request, Response, ResponsePagination } from '@/utils/types/api';
-import { buildQueryString } from '@/utils/formatter';
+import { IBook, BookingSchema } from "./types";
+import axiosWithConfig from "@/utils/apis/axiosWithConfig";
+import { Request, Response, ResponsePagination } from "@/utils/types/api";
+import { buildQueryString } from "@/utils/formatter";
 
 export const getBooking = async (params?: Request) => {
   try {
     const query = buildQueryString(params);
-    const url = query ? `/v1/booking/list${query}&rp=10` : '/v1/booking/list';
+    const url = query ? `/v1/booking/list${query}&rp=10` : "/v1/booking/list";
 
     const response = await axiosWithConfig.get(url);
 
@@ -55,6 +55,20 @@ export const deleteBooking = async (booking_id: string) => {
     );
 
     return response.data as Response;
+  } catch (error: any) {
+    const { messages } = error.response.data;
+
+    throw Error(messages[0]);
+  }
+};
+
+export const cancelBooking = async (booking_id: string) => {
+  try {
+    const response = await axiosWithConfig.post(
+      `/v1/booking/${booking_id}/cancel`
+    );
+
+    return response.data as Response<null>;
   } catch (error: any) {
     const { messages } = error.response.data;
 
